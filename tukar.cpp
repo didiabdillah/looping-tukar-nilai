c831061eb5d7649bda71ee11036f64da9bc1bdbc
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-10-06 10:21:46 +0700

    membuat method prosedur sorting()

--- tukar.cpp
+++ tukar.cpp
@@ -3,33 +3,15 @@
 #include <iomanip>
 using namespace std;
 
-int main()
+void sorting(int banyak, int u, int A[])
 {
-    //deklarasi variabel statis
-    int j, k, i, temp;
-    int jmax, banyak;
-
-    //memasukkan banyak nilai yang ditampung
-    cout << "Masukkan banyak nilai pada elemen array : ";
-    cin >> banyak;
-
-    //deklarasi variabel dinamis
-    int A[banyak];
-    int u = banyak - 1;
-
-    //memasukkan nilai sebelum diurutkan
-    cout << "Masukkan nilai pada elemen array :" << endl;
-    for (i = 0; i < banyak; i++)
-    {
-        cout << "A[" << i << "] =";
-        cin >> A[i];
-    }
+    int jmax, temp;
 
     //Proses pengurutan secara menaik (Ascending)
-    for (j = 0; j < banyak; j++)
+    for (int j = 0; j < banyak; j++)
     {
         jmax = 0;
-        for (k = 1; k <= u; k++)
+        for (int k = 1; k <= u; k++)
         {
             if (A[k] > A[jmax])
             {
@@ -43,8 +25,32 @@ int main()
     }
     //menampilkan nilai setelah diurutkan
     cout << "\nNilai setelah diurutkan =" << endl;
-    for (i = 0; i < banyak; i++)
+    for (int i = 0; i < banyak; i++)
     {
         cout << A[i] << " ";
     }
+}
+
+int main()
+{
+    //deklarasi variabel statis
+    int jmax, banyak;
+
+    //memasukkan banyak nilai yang ditampung
+    cout << "Masukkan banyak nilai pada elemen array : ";
+    cin >> banyak;
+
+    //deklarasi variabel dinamis
+    int A[banyak];
+    int u = banyak - 1;
+
+    //memasukkan nilai sebelum diurutkan
+    cout << "Masukkan nilai pada elemen array :" << endl;
+    for (int i = 0; i < banyak; i++)
+    {
+        cout << "A[" << i << "] =";
+        cin >> A[i];
+    }
+
+    sorting(banyak, u, A);
 }
